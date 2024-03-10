2c73e53d180a8fa390cbd246b4e5c14bf8856aa0
ja3farr <jy502550@gmail.com>
2024-03-10 21:12:32 +0300

    some changes on templates and views

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    context = {
+        'posts' : posts,
+    }
+    return render(request, 'blog/post_list.html', context=context)
