34312a640d7f14ca446a9727955ee55ed8735000
ja3farr <jy502550@gmail.com>
2024-03-17 21:39:40 +0300

    login&logout feature create&remove comments

--- miniblog/blog/models.py
+++ miniblog/blog/models.py
@@ -8,9 +8,27 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    def published(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
     def __str__(self):
         return self.title
+    def approved_comments(self):
+        return self.comments.filter(is_banned=False)
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=32)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_banned = models.BooleanField(default=False)
+    def __str__(self):
+        return self.text
+    def ban(self):
+        self.is_banned = True
+        self.save()
+
+    
     
